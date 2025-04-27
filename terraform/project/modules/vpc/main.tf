4433632c65b85434efc9a4c4aff40f42650b385b
Luiz Fávio Sidral <126919151+SidralLuiz@users.noreply.github.com>
2025-04-27 12:09:46 -0300

    Update main.tf

--- terraform/project/modules/vpc/main.tf
+++ terraform/project/modules/vpc/main.tf
@@ -1,38 +1,38 @@
-data "aws_vpc" "vpc_grupo_d" {
-  id = "vpc-09706dbcf9fda1bc7"  # ID da sua VPC
+data "aws_vpc" "sidral_vpc" {
+  id = "vpc-0bec9b838f67e4b57"  # ID da sua VPC
 }
 
 
-resource "aws_subnet" "sn_pub01" {
-  vpc_id = data.aws_vpc.vpc_grupo_d.id
+resource "aws_subnet" "sidral-sn_pub01" {
+  vpc_id = data.aws_vpc.sidral_vpc.id
   cidr_block = "172.102.1.0/24"
   availability_zone = "us-west-1b"
   tags = {
-    Name = "grupo_d-sn_pub01"
+    Name = "sidral-sn-pub01"
   }
 }
 
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = data.aws_vpc.vpc_grupo_d.id
+resource "aws_internet_gateway" "sidral-igw" {
+  vpc_id = data.aws_vpc.sidral_vpc.id
   tags = {
-    Name = "grupo_d_igw" 
+    Name = "sidral_igw" 
   }
 }
 
-resource "aws_route_table" "route_pub" {
-  vpc_id = data.aws_vpc.vpc_grupo_d.id
+resource "aws_route_table" "sidral-route_pub" {
+  vpc_id = data.aws_vpc.sidral_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.sidral-igw.id
   }
   tags = {
-    Name = "grupo_d-routetable"
+    Name = "sidral-routetable"
   }
 }
 
 resource "aws_route_table_association" "pub01assoc" {
-  subnet_id = aws_subnet.sn_pub01.id
-  route_table_id = aws_route_table.route_pub.id
+  subnet_id = aws_subnet.sidral-sn_pub01.id
+  route_table_id = aws_route_table.sidral-route_pub.id
 }
 
