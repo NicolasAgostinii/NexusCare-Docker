289b732b389fe3ad95684613bc7f1c9b0f62e90d
Luiz Fávio Sidral <126919151+SidralLuiz@users.noreply.github.com>
2025-04-27 12:05:36 -0300

    Update main.tf

--- terraform/project/main.tf
+++ terraform/project/main.tf
@@ -1,5 +1,8 @@
-data "aws_vpc" "vpc_grupo_d" {
-  id = "vpc-09706dbcf9fda1bc7"  # ID da sua VPC
+resource "aws_vpc" "sidral_vpc" {
+  cidr_block = "172.102.0.0/16"
+  tags = {
+    Name = "sidral-vpc"
+  }
 }
 module "vpc" {
     source = "./modules/vpc"
@@ -7,26 +10,26 @@ module "vpc" {
 }
 module "loadbalancer" {
     source = "./modules/loadbalancer"
-    vpc_id = data.aws_vpc.vpc_grupo_d.id
+    vpc_id = aws_vpc.vpc_sidral.id
     sn_pub01 = module.vpc.sn_pub01
 
 }
 
 module "backend" {
     source = "./modules/backend"
-    vpc_id = data.aws_vpc.vpc_grupo_d.id
+    vpc_id = aws_vpc.vpc_sidral.id
    sn_pub01 = module.vpc.sn_pub01
  
 }
 
 module "database" {
     source = "./modules/database"
-    vpc_id = data.aws_vpc.vpc_grupo_d.id
+    vpc_id = aws_vpc.vpc_sidral.id
     sn_pub01 = module.vpc.sn_pub01
 }
 
 module "frontend" {
     source = "./modules/frontend"
-    vpc_id = data.aws_vpc.vpc_grupo_d.id
+    vpc_id = aws_vpc.vpc_sidral.id
     sn_pub01 = module.vpc.sn_pub01
 }
