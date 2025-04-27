808ba9bbf28281fdaa86ca06a3bcc1acf78960fd
Luiz Fávio Sidral <126919151+SidralLuiz@users.noreply.github.com>
2025-04-27 12:22:30 -0300

    Update main.tf

--- terraform/project/modules/frontend/main.tf
+++ terraform/project/modules/frontend/main.tf
@@ -7,17 +7,17 @@ data "aws_ami" "imagem_ec2" {
     }
 }
 
-data "aws_security_group" "grupo_d_sg" {
+data "aws_security_group" "sidral_sg" {
   filter {
     name   = "group-name"         # O campo que queremos filtrar (o nome do SG)
-    values = ["sg_grupo_d"]        # O nome real do seu SG
+    values = ["sidral_sg"]        # O nome real do seu SG
   }
 }
 
 resource "aws_network_interface" "grupo_d_frontend_ei" {
   subnet_id = var.sn_pub01
   tags = {
-    Name = "grupo_d_frontend_ei"
+    Name = "sidral_frontend"
   }
 }
 
@@ -25,8 +25,8 @@ resource "aws_instance" "grupo_d_frontend_ec2" {
   instance_type = "t3.micro"
   ami = data.aws_ami.imagem_ec2.id
   subnet_id = var.sn_pub01
-  vpc_security_group_ids = [data.aws_security_group.grupo_d_sg.id]
-  key_name =  data.aws_key_pair.lb_ssh_key_pair_grupo_d.key_name
+  vpc_security_group_ids = [data.aws_security_group.sidral_sg.id]
+  key_name =  data.aws_key_pair.lb_ssh_key_pair_sidral.key_name
   associate_public_ip_address = true
   tags = {
     Name = "grupo_d-frontend_ec2"
@@ -46,7 +46,7 @@ resource "aws_instance" "grupo_d_frontend_ec2" {
 #  algorithm = "RSA"
 #  rsa_bits  = 2048
 #}
-data "aws_key_pair" "lb_ssh_key_pair_grupo_d" {
-  key_name   = "grupo_d_key_pare"
- # public_key = tls_private_key.lb_ssh_key_frontend.public_key_openssh
+data "aws_key_pair" "lb_ssh_key_pair_sidral" {
+  key_name   = "sidral_key_pair"
+
 }
