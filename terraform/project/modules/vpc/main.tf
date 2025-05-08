a4ea19f9f5698e4d2ee2c146a998a8ed550f18c6
Luiz Fávio Sidral <126919151+SidralLuiz@users.noreply.github.com>
2025-05-08 20:19:44 -0300

    Update main.tf

--- terraform/project/modules/vpc/main.tf
+++ terraform/project/modules/vpc/main.tf
@@ -1,38 +1,38 @@
-data "aws_vpc" "sidral_vpc" {
-  id = "vpc-0bec9b838f67e4b57"  # ID da sua VPC
+data "aws_vpc" "nexus_vpc" {
+  id = "vpc-0326b5a4ed4695435"  # ID da sua VPC
 }
 
 
-resource "aws_subnet" "sidral-sn_pub01" {
-  vpc_id = data.aws_vpc.sidral_vpc.id
+resource "aws_subnet" "nexus-sn_pub01" {
+  vpc_id = data.aws_vpc.nexus_vpc.id
   cidr_block = "172.102.11.0/24"
   availability_zone = "us-west-1b"
   tags = {
-    Name = "sidral-sn-pub01"
+    Name = "nexus-sn-pub01"
   }
 }
 
 
-resource "aws_internet_gateway" "sidral-igw" {
-  vpc_id = data.aws_vpc.sidral_vpc.id
+resource "aws_internet_gateway" "nexus-igw" {
+  vpc_id = data.aws_vpc.nexus_vpc.id
   tags = {
-    Name = "sidral_igw" 
+    Name = "nexus_igw" 
   }
 }
 
-resource "aws_route_table" "sidral-route_pub" {
-  vpc_id = data.aws_vpc.sidral_vpc.id
+resource "aws_route_table" "nexus-route_pub" {
+  vpc_id = data.aws_vpc.nexus_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.sidral-igw.id
+    gateway_id = aws_internet_gateway.nexus-igw.id
   }
   tags = {
-    Name = "sidral-routetable"
+    Name = "nexus-routetable"
   }
 }
 
 resource "aws_route_table_association" "pub01assoc" {
-  subnet_id = aws_subnet.sidral-sn_pub01.id
-  route_table_id = aws_route_table.sidral-route_pub.id
+  subnet_id = aws_subnet.nexus-sn_pub01.id
+  route_table_id = aws_route_table.nexus-route_pub.id
 }
 
