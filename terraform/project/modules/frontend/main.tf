fd84d5be5f7a2a5008453edb5085552756aa500e
Luiz Fávio Sidral <126919151+SidralLuiz@users.noreply.github.com>
2025-05-08 20:26:15 -0300

    Update main.tf

--- terraform/project/modules/frontend/main.tf
+++ terraform/project/modules/frontend/main.tf
@@ -7,17 +7,17 @@ data "aws_ami" "imagem_ec2" {
     }
 }
 
-data "aws_security_group" "sidral_sg" {
+data "aws_security_group" "nexus_sg" {
   filter {
     name   = "group-name"         # O campo que queremos filtrar (o nome do SG)
-    values = ["sidral_sg"]        # O nome real do seu SG
+    values = ["nexus_sg"]        # O nome real do seu SG
   }
 }
 
 resource "aws_network_interface" "grupo_d_frontend_ei" {
   subnet_id = var.sn_pub01
   tags = {
-    Name = "sidral_frontend"
+    Name = "nexus_frontend"
   }
 }
 
@@ -25,21 +25,15 @@ resource "aws_instance" "grupo_d_frontend_ec2" {
   instance_type = "t3.micro"
   ami = data.aws_ami.imagem_ec2.id
   subnet_id = var.sn_pub01
-  vpc_security_group_ids = [data.aws_security_group.sidral_sg.id]
-  key_name =  data.aws_key_pair.lb_ssh_key_pair_sidral.key_name
+  vpc_security_group_ids = [data.aws_security_group.nexus_sg.id]
+  key_name =  data.aws_key_pair.lb_ssh_key_pair_nexus.key_name
   associate_public_ip_address = true
   tags = {
-    Name = "sidral_frontend"
+    Name = "nexus_frontend"
   }
   
 }
-
-# Criacao da chave SSH que sera usada para conexao na instancia
-#resource "tls_private_key" "lb_ssh_key" {
-#  algorithm = "RSA"
-#  rsa_bits  = 2048
-#}
-data "aws_key_pair" "lb_ssh_key_pair_sidral" {
-  key_name   = "sidral_key_pair"
+data "aws_key_pair" "lb_ssh_key_pair_nexus" {
+  key_name   = "nexus_key_pair"
 
 }
