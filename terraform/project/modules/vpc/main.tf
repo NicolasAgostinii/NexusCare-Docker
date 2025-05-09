eb5082760081e0e484fa2dbfb3cbe11df3cbf9a1
Luiz Fávio Sidral <126919151+SidralLuiz@users.noreply.github.com>
2025-05-09 18:02:10 -0300

    Update main.tf

--- terraform/project/modules/vpc/main.tf
+++ terraform/project/modules/vpc/main.tf
@@ -3,9 +3,14 @@ data "aws_vpc" "nexus_vpc" {
 }
 
 
-data "aws_subnet" "nexus_subnetpub01"{
-id = "subnet-0c166bf0a23a66867"
+resource "aws_subnet" "sn_pub01" {
+  vpc_id = aws_vpc.grupo_d_vpc.id
+  cidr_block = "172.102.3.0/24"
+  availability_zone = "us-west-1b"
+  tags = {
+    Name = "grupo_d-sn_pub01"
   }
+}
 
 resource "aws_internet_gateway" "nexus-igw" {
   vpc_id = data.aws_vpc.nexus_vpc.id
